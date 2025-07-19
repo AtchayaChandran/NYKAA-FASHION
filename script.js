7b093e6ff7b42772e4b90feb770289b566f60681
Atchaya Chandran <achurama2003@gmail.com>
2025-07-20 05:16:56 +0530

    Updated fully

---
+++ script.js
@@ -0,0 +1,10 @@
+/*-------For POP UP--------*/
+function closePopup() {
+   document.getElementById("entryPopup").style.display = "none";
+}
+
+
+function toggleNykaaSection() {
+   const section = document.getElementById('nykaaContent');
+   section.classList.toggle('hidden');
+}
