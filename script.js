89f72060dd772deecc053db77a390523ce83f214
Atchaya Chandran <achurama2003@gmail.com>
2025-07-21 04:49:44 +0530

    Added categorie

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ function closePopup() {
    document.getElementById("entryPopup").style.display = "none";
 }
 
-
+/*---------------for arrow------------------*/
 function toggleNykaaSection() {
    const section = document.getElementById('nykaaContent');
    section.classList.toggle('hidden');
+}
+
+/*-----------for menubar-------------------*/
+function openMenu() {
+   document.getElementById('sideMenu').style.left = '0';
+   document.getElementById('backdrop').classList.remove('hidden');
+}
+
+function closeMenu() {
+   document.getElementById('sideMenu').style.left = '-100%';
+   document.getElementById('backdrop').classList.add('hidden');
 }
